5e71754463aad8f7787f500d18558758b8e5eca7
Michael Grischenko <robotcrimefighter@gmail.com>
2017-02-14 10:47:28 -0600

    cleaned up AppComponent

--- src/components/AppComponent.js
+++ src/components/AppComponent.js
@@ -2,44 +2,17 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-import {clone, find} from 'lodash';
 import RatesDisplay from './RatesDisplayComponent'
 
 class AppComponent extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = clone(this.props.params);
-  }
-
-  componentDidUpdate(oldProps) {
-    if (this.haveParamsChanged(oldProps)) {
-      this.updateState();
-    }
-  }
-
-  haveParamsChanged(oldProps) {
-    return find([
-      'base',
-      'compare',
-      'date',
-      'days'
-    ], (key) => {
-      return this.props.params[key] !== oldProps.params[key];
-    });
-  }
-
-  updateState() {
-    this.setState(clone(this.props.params));
-  }
-
   render() {
     return (
       <div className="index">
         <RatesDisplay
-          base={this.state.base}
-          compare={this.state.compare}
-          date={this.state.date}
-          days={this.state.days}
+          base={this.props.params.base}
+          compare={this.props.params.compare}
+          date={this.props.params.date}
+          days={this.props.params.days}
         ></RatesDisplay>
       </div>
     );
